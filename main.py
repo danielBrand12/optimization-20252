8eb75562a63f025faef73db627a7942494b4ed32
Daniel <danielbrandtaborda@gmail.com>
2025-08-30 17:33:04 -0700

    Initialize project structure with main functionality and methods for optimization techniques

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from optimization-20252!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ metodos/falsa_posicion.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def f(x):
+  return 3*x**2 - 120*x + 100
+
+def falsa_posicion(f, xl, xu, error):
+  results = pd.DataFrame(columns=['xl', 'xu', 'xr', 'error'])
+  iteraciones = 0
+
+  if f(xl)*f(xu) > 0:
+    print("No hay raiz en ese intervalo")
+    return
+
+  while abs(xu - xl) > error:
+    xr = xu - ((f(xu)*(xl - xu))/(f(xl) - f(xu)))
+    results.loc[len(results)] = [xl, xu, xr, abs(xu - xl)]
+    if f(xr) == 0:
+      results.loc[len(results)] = [xl, xu, xr, abs(xu - xl)]
+
+      return xr, iteraciones, results
+    elif f(xl)*f(xr) < 0:
+      xu = xr
+    else:
+      xl = xr
+    iteraciones += 1
+
+
+  return xr, iteraciones, results
+
+
+xl = -10
+
+xu = 20
+
+error = 0
+
+raiz, iteraciones, df = falsa_posicion(f, xl, xu, error)
+
+print("La raiz es: ", raiz)
+print("Se realizaron ", iteraciones, " iteraciones")
+print("La tabla de valores es: ")
+print(df)
+
+plt.plot(df['error'])
+plt.xlabel('Iteración')
+plt.ylabel('Error')
+plt.title('Comportamiento del error en el método de bisección')
+plt.show()
